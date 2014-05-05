95e492ebf52939e46214ab9234233fe175648c09
Chris Violette <cvioletterun@gmail.com>
2014-05-05 11:29:30 -0400

    Show single creator

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,2 +1,26 @@
 class CreatorsController < ApplicationController
+  def new
+    @creator = Creator.new
+  end
+
+  def create
+    @creator = Creator.new(creator_params)
+    if @creator.save
+      flash[:success] = "Welcome to WeAreBGR!"
+      redirect_to @creator
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @creator = Creator.find(params[:id])
+  end
+
+  private
+
+    def creator_params
+      params.require(:creator).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Wearebgr::Application.routes.draw do
+  resources :creators
   root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
