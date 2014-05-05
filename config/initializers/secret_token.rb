f029a5625b84913da7445978174c4ca39794e87c
Chris Violette <cvioletterun@gmail.com>
2014-05-05 10:27:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wearebgr::Application.config.secret_key_base = 'eb89504948184d92d7ed615e0c16d6b17632159822dced0055cb64d2d6c8ec9692648eae6f7af7b0c4e37cefe498dc259ab2c6fcf78400ad8de8ada88e85576c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wearebgr::Application.config.session_store :cookie_store, key: '_wearebgr_session'
