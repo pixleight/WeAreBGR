b1437570d6dcf6036c9941c7313e0f9cf22e1fb3
Chris Violette <cvioletterun@gmail.com>
2014-05-09 14:41:09 -0400

    Added some stuff for user avatars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_filter :get_all_tags
+
+  def get_all_tags
+    @alltags = ActsAsTaggableOn::Tag.all
+  end
 end
