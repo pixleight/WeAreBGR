bc285d6a2ebc7896c0c799a36dc349a2645867c1
Chris Violette <cvioletterun@gmail.com>
2014-05-23 22:36:13 -0400

    Updated signin redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     if creator && creator.authenticate(params[:session][:password])
       # Sign in
       sign_in creator
-      redirect_to creator
+      flash[:success] = "Welcome back, #{creator.name}!"
+      redirect_to root_path
     else
       # Create an error message & re-render the signin form
       flash.now[:alert] = 'Invalid email/password combination'
@@ -17,6 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out
+    flash[:success] = "Successfully signed out."
     redirect_to root_url
   end
 end
